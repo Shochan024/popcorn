86a64d14001ea8e0cd2c2fb983db3d249b8444a9
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-09-27 01:59:23 +0900

    decision tree importance graph added

--- lib/predict.py
+++ lib/predict.py
@@ -5,6 +5,7 @@ import json
 import pydotplus
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from abc import ABCMeta , abstractmethod
 from PIL import Image
@@ -12,6 +13,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import plot_tree
 from .tools.logger import *
 from .tools.file_modules import *
+sns.set()
+sns.set(font='Yu Gothic')
+sns.set( font=["IPAexGothic"], font_scale=0.8 )
 
 __all__ = ["decisiontree"]
 
@@ -51,19 +55,38 @@ class decisiontree(Prediction):
         model.fit( X , Y )
 
         if self.save is True:
-            fig = plt.figure()
-            ax = fig.add_subplot()
-            plot_tree( model , feature_names=self.x_cols , ax=ax ,\
-             class_names=np.unique( Y ).astype(np.str) , filled=True)
+            # Plot Decision Tree
+            self.__tree_plot( model=model , X=X , Y=Y )
 
-            filename = os.path.dirname( self.filename.replace( "datas" , "graphs" ) )
-            filename = filename + "/decisiontree_{}_{}.png".\
-            format( self.y_cols[0] , "_".join( self.x_cols ) )
-            if os.path.exists( os.path.dirname( filename ) ) is not True:
-                system( "mkdir {}".format( os.path.dirname( filename ) ) )
-                os.makedirs( os.path.dirname( filename ) )
-
-            system( "saved Decision Tree image as {}".format( filename ) )
-            plt.savefig( filename )
+            # Plot feature importance
+            self.__importance_plot( model=model )
 
         return model
+
+    def __tree_plot( self , model , X , Y ):
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        plot_tree( model , feature_names=self.x_cols , ax=ax ,\
+         class_names=np.unique( Y ).astype(np.str) , filled=True)
+
+        filename = os.path.dirname( self.filename.replace( "datas" , "graphs" ) )
+        filename = filename + "/decisiontree_{}_{}.png".\
+        format( self.y_cols[0] , "_".join( self.x_cols ) )
+        if os.path.exists( os.path.dirname( filename ) ) is not True:
+            system( "mkdir {}".format( os.path.dirname( filename ) ) )
+            os.makedirs( os.path.dirname( filename ) )
+
+        system( "saved Decision Tree image as {}".format( filename ) )
+        plt.savefig( filename )
+
+    def __importance_plot( self , model ):
+        plt.cla()
+        fig = plt.figure()
+        ax = fig.add_subplot()
+        plt.xlim( 0 , 1 )
+        filename = os.path.dirname( self.filename.replace( "datas" , "graphs" ) )
+        filename = filename + "/decisiontree_{}_{}_importance.png".\
+        format( self.y_cols[0] , "_".join( self.x_cols ) )
+        plt.barh( self.x_cols , model.feature_importances_ )
+        system( "saved Decision Tree feature importance image as {}".format( filename ) )
+        plt.savefig( filename )
