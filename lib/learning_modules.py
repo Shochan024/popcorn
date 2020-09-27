c5078674a67f771c15d6dcb0740c210cb56faa21
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-09-27 21:48:23 +0900

    calibration_curve plot added

--- lib/learning_modules.py
+++ lib/learning_modules.py
@@ -17,6 +17,7 @@ from .tools.file_modules import *
 from abc import ABCMeta , abstractmethod
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.calibration import calibration_curve
 from sklearn.model_selection import train_test_split
 
 
@@ -212,8 +213,42 @@ class logistic(Learning,LearnController):
         report_dict = self.acc_calc( model=model , df=self.df , query=self.query ,\
          x_cols=self.x_cols , y_cols=self.y_cols )
 
+        self.__plot_calibration( df=self.df , query=self.query , model=model ,\
+         x_cols=self.x_cols , y_cols=self.y_cols )
+
         return report_dict
 
     def dump( self , model ):
         self.model_save( model=model , filename=self.filename ,\
          modelname="logistic" , x_cols=self.x_cols , y_cols=self.y_cols )
+
+    def __plot_calibration( self , df , query , model , x_cols , y_cols ):
+        if query != "":
+            df = df.query( query )
+
+        X = df[x_cols]
+        Y = df[y_cols]
+        X_train , X_test , Y_train , Y_test = train_test_split( X , Y )
+
+        prob = model.predict_proba( X_test )[:,1]
+        prob_true , prob_pred = calibration_curve( y_true=Y_test ,\
+         y_prob=prob , n_bins=40 )
+
+        fig = plt.figure()
+        ax1 = plt.subplot(2,1,1)
+        ax1.plot( prob_pred , prob_true , marker="s" , label="calibration_curve" )
+        ax1.plot( [0,1],[0,1],linestyle="--",label="ideal" )
+        ax1.legend()
+
+        ax2 = plt.subplot(2,1,2)
+        ax2.hist( prob , bins=40 , histtype="step" )
+        ax2.set_xlim(0,1)
+        filename = os.path.dirname( self.filename.replace( "datas" , "graphs" ) )
+        filename = filename + "/calibration_curve{}_{}.png".\
+        format( self.y_cols[0] , "_".join( self.x_cols ) )
+        if os.path.exists( os.path.dirname( filename ) ) is not True:
+            message( "mkdir {}".format( os.path.dirname( filename ) ) )
+            os.makedirs( os.path.dirname( filename ) )
+
+        message( "saved calibration_curve image as {}".format( filename ) )
+        plt.savefig( filename )
