b84fab5d6c6cbc29008481adbaa29f482f774881
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-09-27 09:45:11 +0900

    filename refactored

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,13 +1,13 @@
 #!-*-coding:utf-8-*-
 from .csv_modules import *
-from .describes import *
-from .learn import *
+from .graph_modules import *
+from .learning_modules import *
 from .tools.data_modules import *
 from .tools.file_modules import *
 __all__ = []
 __all__ += csv_modules.__all__
-__all__ += describes.__all__
-__all__ += learn.__all__
+__all__ += graph_modules.__all__
+__all__ += learning_modules.__all__
 
 # From tools
 __all__ += tools.data_modules.__all__
